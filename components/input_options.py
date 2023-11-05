42cf8ea07650653e58bfdeead316860e8c7b8eb3
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-11-05 18:34:16 +0200

    Added label at OptionPageContent

--- components/input_options.py
+++ components/input_options.py
@@ -6,18 +6,33 @@ class OptionsPageContent(customtkinter.CTkFrame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
-        options_container = customtkinter.CTkFrame(self)
-        options_container.grid(row=0, column=0, padx=100, pady=(200,0))  # Adjust padx and pady as needed
+        #Fonts
+        tab_font_style = customtkinter.CTkFont(size=25)
+        tab_content_style = customtkinter.CTkFont(size=20)
 
-        options_tab = customtkinter.CTkTabview(options_container)
-        options_tab.grid(row=0, column=0, sticky="nsew")
-        options_tab.configure(width=600) 
+        options_container = customtkinter.CTkFrame(self)
+        options_container.grid(row=0, column=0, pady=(50,0)) 
+
+        # Label and tab frame
+        frame_with_label = customtkinter.CTkFrame(options_container)
+        frame_with_label.grid(row=0, column=0, sticky="nsew")
+
+        choose_method_label = customtkinter.CTkLabel(frame_with_label, text="Choose an input method", font=("sans-serif", 30))
+        choose_method_label.grid(row=0, column=0, padx=150, pady=(0,50), sticky="w")
+
+        # Grid of the whole tab
+        options_tab = customtkinter.CTkTabview(frame_with_label)
+        options_tab.grid(row=1, column=0, sticky="nsew")
+        options_tab.configure(width=600)
+
+        #Tab Styling
+        options_tab._segmented_button.configure(font=tab_font_style)
 
         file_tab = options_tab.add("MP3 File")
-        file_tab_label = customtkinter.CTkLabel(file_tab, text="Content of Tab 1")
+        file_tab_label = customtkinter.CTkLabel(file_tab, text="Content of Tab 1", font=tab_content_style)
 
         mic_tab = options_tab.add("Microphone")
-        mic_tab_label = customtkinter.CTkLabel(mic_tab, text="Content of Tab 2")
+        mic_tab_label = customtkinter.CTkLabel(mic_tab, text="Content of Tab 2", font=tab_content_style)
 
         file_tab_label.pack(fill="both", expand=True)
         mic_tab_label.pack(fill="both", expand=True)
