edf616390364a43a4ecdbe6447c65f721a48de51
Ermina Trontzou <7erminatrontzou7@gmail.com>
2024-08-10 21:43:15 +0300

    Applied some kind of filtering but still needs work

--- components/fft.py
+++ components/fft.py
@@ -3,6 +3,7 @@ import scipy.io.wavfile as wavfile
 import scipy.signal
 import os
 from .lilypond_convert import LilyPondConverter
+import math
 
 class PlayedNote:
     def __init__(self, note, duration):
@@ -10,18 +11,14 @@ class PlayedNote:
         self.duration = duration
 
 class FFT:
-    def __init__(self, fps=5, fft_window_seconds=0.20, freq_min=10, freq_max=1000):
-        self.fps = fps
+    def __init__(self, fft_window_seconds=0.20, freq_min=27.5, freq_max=4186.0, cutoff_freq=27.5, filter_order=32):
         self.fft_window_seconds = fft_window_seconds
         self.freq_min = freq_min
         self.freq_max = freq_max
-       
-        self.frame_step = None
+        self.cutoff_freq = cutoff_freq
+        self.filter_order = filter_order
+        
         self.fft_window_size = None
-        self.frame_count = None
-        self.frame_offset = None
-        self.audio_length = None
-        self.final_notes_array = []
         self.energy_threshold = 1e-4 
         self.rolling_window_size = 10
 
@@ -38,29 +35,18 @@ class FFT:
         return note + octave
 
     def extract_sample(self, audio, frame_number):
-        begin = frame_number * self.frame_offset
+        begin = frame_number * self.fft_window_size
         end = begin + self.fft_window_size
         if end > len(audio):
             end = len(audio)
             return np.concatenate([audio[begin:], np.zeros((self.fft_window_size - (end - begin)), dtype=float)])
 
         return audio[begin:end]
-
-        # end = frame_number * self.frame_offset
-        # begin = int(end - self.fft_window_size)
-
-        # if end == 0:
-        #     return np.zeros((np.abs(begin)), dtype=float)
-        # elif begin < 0:
-        #     return np.concatenate([np.zeros((np.abs(begin)), dtype=float), audio[0:end]])
-        # else:
-        #     return audio[begin:end]
         
     def gcd(self, frequencies):
         """Calculate GCD among a list of frequencies."""
-        # Check if the frequencies list is empty
         if not frequencies:
-            return None  # Return None if the list is empty
+            return None
         
         def euclidean_gcd(a, b):
             """Euclidean algorithm for GCD."""
@@ -74,16 +60,12 @@ class FFT:
             gcd_freq = euclidean_gcd(gcd_freq, freq)
         return gcd_freq / 1e6
   
+    def high_pass_filter(self, audio, fs):
+        sos = scipy.signal.butter(N=self.filter_order, Wn=self.cutoff_freq, fs=fs, btype='highpass', analog=False, output='sos')
+        return scipy.signal.sosfiltfilt(sos=sos, x=audio)
 
     def process_audio_file(self, file_path):
-
-        # assert self.gcd([100.0, 100.0]) == 100.0, "Test Case 1 Failed"
         assert self.gcd([292.0, 584.0, 876.0]) == 292.0, "Test Case 2 Failed"
-        # assert self.gcd([300.0]) == 300.0, "Test Case 3 Failed"
-        # assert self.gcd([13.0, 26.0]) == 13.0, "Test Case 4 Failed"
-        # assert self.gcd([]) == None, "Test Case 5 Failed"
-        # print("All test cases passed.")
-
 
         fs, data = wavfile.read(file_path)
     
@@ -91,23 +73,23 @@ class FFT:
             audio = data
         else:
             audio = data.T[0]
+
+        # Apply high-pass filter
+        audio = self.high_pass_filter(audio, fs)
         
-        self.frame_step = (fs / self.fps)
         self.fft_window_size = int(fs * self.fft_window_seconds)
-        print(f"FFT windows size of {self.fft_window_seconds} sec leads to FFT windows size of {self.fft_window_size} samples")
-        self.audio_length = len(audio) / fs
+        total_frames = math.ceil(len(audio) / self.fft_window_size)
+        print(f"FFT window size of {self.fft_window_seconds} sec leads to FFT window size of {self.fft_window_size} samples for {total_frames} frames in the file")
 
         window = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, self.fft_window_size, False)))
         xf = np.fft.rfftfreq(self.fft_window_size, 1 / fs)
-        self.frame_count = int(self.audio_length * self.fps)
-        self.frame_offset = int(len(audio) / self.frame_count)
 
         energy_list = []
         notes_info = []
         current_played_note = PlayedNote(note=None, duration=0)
         notes = []
 
-        for frame_number in range(self.frame_count):
+        for frame_number in range(total_frames):
             sample = self.extract_sample(audio, frame_number)
             fft_result = np.fft.rfft(sample * window)
             fft_magnitude = np.abs(fft_result).real
@@ -123,62 +105,52 @@ class FFT:
             current_note = None
             if frame_energy < avg_energy * 0.1:
                 current_note = "pause"
+                print (f"Frame {frame_number}: Pause with energy {frame_energy} < {avg_energy} * 0.1")
             else:
-                peaks, properties = scipy.signal.find_peaks(fft_magnitude, height=1500.00)
-                valid_peaks = [peak for peak in peaks if 16.35 < xf[peak] < 7902.13]
+                peaks, properties = scipy.signal.find_peaks(fft_magnitude, height=50000.00)
             
-                if len(valid_peaks) == 0:
+                if len(peaks) == 0:
                     current_note = "pause"
+                    print (f"Frame {frame_number}: Pause with no peaks")
                 else:
                     main_peak_index = np.argmax(properties['peak_heights'])
-                    fundamental_freq = xf[valid_peaks[main_peak_index]]
+                    fundamental_freq = xf[peaks[main_peak_index]]
                     fundamental_amplitude = properties['peak_heights'][main_peak_index]
 
-                    dynamic_threshold = 0.05 * fundamental_amplitude
+                    dynamic_threshold = 0.1 * fundamental_amplitude
                     print(f"Frame {frame_number}: Fundamental frequency = {fundamental_freq}, Amplitude = {fundamental_amplitude}, Dynamic Threshold = {dynamic_threshold}")
 
                     close_harmonics = []
-                    for peak in valid_peaks:
+                    for peak in peaks:
                         potential_harmonic_freq = xf[peak]
 
                         if fft_magnitude[peak] < dynamic_threshold:
                             continue
-                        # close_harmonics.append(potential_harmonic_freq)
-
-                        # if potential_harmonic_freq <= fundamental_freq:
-                        #     continue
 
                         if fundamental_freq < potential_harmonic_freq:
                             deviation_from_harmony = potential_harmonic_freq / fundamental_freq - round(potential_harmonic_freq / fundamental_freq)
-                            if abs(deviation_from_harmony) <= 0.05:
+                            if abs(deviation_from_harmony) <= 0.02:
                                 theoretical_harmonic = round(potential_harmonic_freq / fundamental_freq) * fundamental_freq
                                 close_harmonics.append(theoretical_harmonic)
                         else:
                             deviation_from_harmony = fundamental_freq / potential_harmonic_freq - round(fundamental_freq / potential_harmonic_freq)
-                            if abs(deviation_from_harmony) <= 0.03:
+                            if abs(deviation_from_harmony) <= 0.05:
                                 theoretical_harmonic = fundamental_freq / round(fundamental_freq / potential_harmonic_freq)
                                 close_harmonics.append(theoretical_harmonic)
-                        
-                        # if potential_harmonic_freq < fundamental_freq:
-                        #     deviation_from_harmony = fundamental_freq / potential_harmonic_freq - round(fundamental_freq / potential_harmonic_freq)
-                        #     if abs(deviation_from_harmony) <= 0.05:
-                        #         theoretical_harmonic = fundamental_freq / round(fundamental_freq / potential_harmonic_freq)
-                        #         close_harmonics.append(theoretical_harmonic)
-                            
-
-
-                    # print(f"For {fundamental_freq} found possible harmonics {close_harmonics}")
 
                     print(f"After applying threshold, close harmonics for {fundamental_freq}: {close_harmonics}")
 
-
                     base_frequency = self.gcd(close_harmonics + [fundamental_freq])
-                    print(f"Assuming base frequency {base_frequency}")
 
-                    note_number = self.freq_to_number(base_frequency)
-                    print(f"Note number {note_number}")
-                    current_note = self.note_name(note_number)
-                    print(f"Current note {current_note}")
+                    if base_frequency < self.freq_min or base_frequency > self.freq_max:
+                        current_note = "pause"
+                    else:
+                        print(f"Assuming base frequency {base_frequency}")
+
+                        note_number = self.freq_to_number(base_frequency)
+                        print(f"Note number {note_number}")
+                        current_note = self.note_name(note_number)
+                        print(f"Current note {current_note}")
 
             if notes and current_note == notes[-1].note:
                 notes[-1].duration += self.fft_window_seconds
@@ -187,7 +159,7 @@ class FFT:
 
         for note in notes:
             notes_info.append({"note": note.note, "duration": note.duration})
-
+        
         print('Notes for : ', os.path.basename(file_path), '\n', notes_info)
 
         converter = LilyPondConverter(notes_info)
--- happy-birthday.ly
+++ happy-birthday.ly
@@ -1,4 +1,7 @@
 \version "2.20.0"
 {
-r1 g2 a2 g2 c'2 b16 r8 b1 r2 a2 g16 r16 g4 d'2 c'1 r2 g16 r16 g8 g'2 e'2 c'4 r16 c'8 b16 r4 b4 a16 r16 a2 f'16 r2 e'2 c'2 r8 d'4 c'1 r1
+<<
+  \new Staff { \clef treble  r1 r16 b'''''16 b''''16 r16 c'''''16 r4 cis'''16 b'''''16 r16 ais''''16 b''''16 a'''8 e'''''16 r4 fis''16 a''''16 dis''''16 b'''''16 r8 dis''''16 r16 r8 a'''''16 c'''''16 r8 cis'''''16 e'''''16 b''''16 r8 d'''16 r2 f''16 r16 gis'16 r16 r16 r16 r16 r4 gis'16 r16 r16 r16 dis''16 r8 c'''''16 r16 r4 e''''16 fis''16 r8 r16 b''''16 f''16 r8 r16 f''16 gis'16 b'16 c'''16 r8 r2 r2 r2 c'2 r1 r2 r2 r2 d'2 c'2 r16 r2 g'2 e'2 c'2 r2 r2 f'2 e'2 c'2 d'2 c'1 r1 }
+  \new Staff { \clef bass r1 b,16 r16 r16 r16 r16 r4 r16 r16 r16 r16 r16 r8 r16 r4 r16 r16 r16 r16 r8 r16 g,16 r8 r16 r16 r8 r16 r16 r16 r8 r16 r2 r16 d16 r16 dis,,,16 r16 d,,16 dis,,,16 r4 r16 r16 dis,,,16 c,,16 r16 r8 r16 c,,16 r4 r16 r16 r8 dis,,,16 r16 r16 r8 dis,,,16 r16 r16 r16 r16 r8 g2 a2 g2 r2 b1 g2 a2 g2 r2 r2 g,16 g2 r2 r2 r2 b2 a2 r2 r2 r2 r2 r1 r1}
+>>
 }
